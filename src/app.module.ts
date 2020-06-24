cb66c90891ceabde6e802111967a629b60a819e8
刘思鑫 <fordreamxkhl@163.com>
2020-06-24 23:35:39 +0800

    chore: update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,25 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminauthMiddleware } from './middleware/adminauth.middleware';
 import { AdminModule } from './module/admin/admin.module';
 import { ApiModule } from './module/api/api.module';
 import { DefaultModule } from './module/default/default.module';
-import { AdminService } from './service/admin/admin.service';
+
+//配置中间件
 
 @Module({
   imports: [
     AdminModule,
-    ApiModule,
     DefaultModule,
+    ApiModule,
     MongooseModule.forRoot('mongodb://localhost/nestshop', {
       useNewUrlParser: true,
     }),
   ],
   controllers: [],
-  providers: [AdminService],
+  providers: [],
 })
-export class AppModule {
+export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(AdminauthMiddleware).forRoutes('admin/*');
   }
