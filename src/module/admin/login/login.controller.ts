c971704b18f01170c49ec0ba50d0c79ba35d222a
刘思鑫 <fordreamxkhl@163.com>
2020-06-28 01:45:10 +0800

    feat: 实现登录、退出登录功能、配置公 共的成功 失败提示

--- src/module/admin/login/login.controller.ts
+++ src/module/admin/login/login.controller.ts
@@ -32,70 +32,39 @@ export class LoginController {
       const code: string = body.code;
       const username: string = body.username;
       let password: string = body.password;
-
       if (username == '' || password.length < 6) {
-        console.log('用户名或者密码不合法');
-        res.redirect('/admin/main');
+        this.toolsService.error(res, '用户名或者密码不合法', '/admin/login');
       } else {
-        if (code.toUpperCase() === req.session.code.toUpperCase()) {
+        if (code.toUpperCase() == req.session.code.toUpperCase()) {
           password = this.toolsService.getMd5(password);
           const userResult = await this.adminService.find({
             username: username,
             password: password,
           });
-
           if (userResult.length > 0) {
             console.log('登录成功');
             req.session.userinfo = userResult[0];
-            res.redirect('/admin/main');
+            this.toolsService.success(res, '/admin/main');
           } else {
-            console.log('用户名或者密码不正确');
-            res.redirect('/admin/login');
+            this.toolsService.error(
+              res,
+              '用户名或者密码不正确',
+              '/admin/login',
+            );
           }
         } else {
-          console.log('验证码不正确');
-          res.redirect('/admin/login');
+          this.toolsService.error(res, '验证码不正确', '/admin/login');
         }
       }
     } catch (error) {
+      console.log(error);
       res.redirect('/admin/login');
     }
   }
-  /*
 
-     注意：1、需要在前端页面用js验证用户输入的信息是否正确     2、后台获取数据以后判断数据格式是否正确
-
-
-
-     1、获取表单提交的数据
-
-     2、判断验证码是否正确
-
-     验证码正确
-
-         1、要对表单里面的密码进行md5加密               md5模块  https://www.npmjs.com/package/md5
-
-                                                           1、安装 cnpm install md5 --save
-
-                                                           2、引入 md5 var md5 = require('md5');
-
-                                                           3、使用  md5(str)
-
-
-         2、在用户表（集合）中查询当前用户是否存在              （mongoose操作mongodb数据库）https://docs.nestjs.com/techniques/mongodb
-
-                                                                 1、配置mongoose
-
-                                                                 2、创建操作数据库的model
-
-
-         3、如果数据库有此用户（登录成功） ：保存用户信息     跳转到后台管理系统
-
-         4、如果数据库有此用户（登录失败）： 跳转到登录页面
-
-
-      验证码错误： 跳转到登录页面   提示验证码不正确
-
-
-     */
+  @Get('loginOut')
+  loginOut(@Req() req, @Res() res) {
+    req.session.userinfo = null;
+    res.redirect('/admin/login');
+  }
 }
--- src/service/tools/tools.service.ts
+++ src/service/tools/tools.service.ts
@@ -18,4 +18,16 @@ export class ToolsService {
   getMd5(str: string) {
     return md5(str);
   }
+
+  async success(res, redirectUrl) {
+    await res.render('admin/public/success', {
+      redirectUrl: redirectUrl,
+    });
+  }
+  async error(res, message, redirectUrl) {
+    await res.render('admin/public/error', {
+      message: message,
+      redirectUrl: redirectUrl,
+    });
+  }
 }
